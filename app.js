5fe5a23d0a6355449321196e8094ba7574ee0186
= <=>
2023-06-02 14:53:37 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const btn1 = document.querySelector('.btn1');
+const btn2 = document.querySelector('.btn2');
+const btn3 = document.querySelector('.btn3');
+
+btn1.onclick = function(){
+    alert('hello world');
+}
+btn2.onclick = function(){
+    alert('olhaaaaa');
+}
+btn3.onclick = function(){
+    alert('CARAAALHOOOOOOOOOOOOOOOOOOOOo');
+}
