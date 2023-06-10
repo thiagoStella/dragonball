553ab877df3836d4d1f627f42f209e0f6396ca03
= <=>
2023-06-10 11:02:17 -0300

    manipulação de img

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const btn1 = document.querySelector('.btn1');
 const btn2 = document.querySelector('.btn2');
 const btn3 = document.querySelector('.btn3');
-
+const goku = document.querySelector('.goku');
 btn1.onclick = function(){
-    alert('hello world');
+    document.querySelector('.goku').src="assets/gokussj1.webp";
 }
 btn2.onclick = function(){
-    alert('olhaaaaa');
+    document.querySelector('.goku').src="assets/gokussj2.jpeg";
 }
 btn3.onclick = function(){
-    alert('CARAAALHOOOOOOOOOOOOOOOOOOOOo');
+    document.querySelector('.goku').src="assets/gokussj3.png";
 }
